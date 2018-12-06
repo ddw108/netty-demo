1b67988366e621d8aa34cc56d3ff7e30d6d37b2a
dengdingwen <dengdingwen001@ke.com>
2018-12-06 17:09:43 +0800

    first commit： 关于netty的一个小demo，实现了客户端往服务端发送消息

---
+++ src/main/java/Client/NettyClient.java
@@ -0,0 +1,74 @@
+package Client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 对于客户端的启动来说，
+ * 和服务端的启动类似，
+ * 依然需要线程模型、IO 模型，以及 IO 业务处理逻辑三大参数
+ *
+ * @author dengdingwwen
+ * @version $Id: NettyClient.java,v 1.0 2018/12/6 15:21 dengdingwwen
+ * @date 2018/12/6 15:21
+ */
+public class NettyClient {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        //客户端引导类
+        Bootstrap bootstrap = new Bootstrap();
+
+        //客户端线程组
+        NioEventLoopGroup group = new NioEventLoopGroup();
+
+        bootstrap
+                //指定线程模型
+                .group(group)
+                //指定 IO 类型为 NIO
+                .channel(NioSocketChannel.class)
+                //IO 处理逻辑
+                .handler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel ch) {
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                });
+
+        //建立连接
+        Channel channel = connect(bootstrap, "127.0.0.1", 8001, 10);
+
+        while (true) {
+            channel.writeAndFlush(new Date() + ": hello world!");
+            Thread.sleep(2000);
+        }
+    }
+
+    private static Channel connect(Bootstrap bootstrap, String host, int port, int retry) {
+        Channel channel = bootstrap.connect(host, port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("连接成功!");
+            } else if (retry == 0) {
+                System.err.println("重试次数已用完，放弃连接！");
+            } else {
+                // 第几次重连
+                int order = (10 - retry) + 1;
+                // 本次重连的间隔
+                int delay = 1 << order;
+                System.err.println(new Date() + ": 连接失败，第" + order + "次重连……");
+                bootstrap.config().group().schedule(() -> connect(bootstrap, host, port, retry - 1), delay, TimeUnit
+                        .SECONDS);
+            }
+        }).channel();
+        return channel;
+    }
+}
---
+++ src/main/java/Service/NettyServer.java
@@ -0,0 +1,75 @@
+package Service;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+
+/**
+ * 创建一个引导类，
+ * 然后给他指定线程模型，IO模型，连接读写处理逻辑，绑定端口之后，
+ * 服务端就启动起来了。
+ *
+ * @author dengdingwwen
+ * @version $Id: NettyServer.java,v 1.0 2018/12/6 15:09 dengdingwwen
+ * @date 2018/12/6 15:09
+ */
+public class NettyServer {
+
+    public static void main(String[] args) {
+
+        //服务端引导类
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+        //boss线程组监听端口，accept新连接的线程组
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        //worker线程组处理每一条连接的数据读写
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+
+        serverBootstrap
+                //配置两大线程组
+                .group(boss, worker)
+                //指定IO模型，这里为NIO模型
+                .channel(NioServerSocketChannel.class)
+                //childHandler()用于指定处理新连接数据的读写处理逻辑
+                //handler()用于指定在服务端启动过程中的一些逻辑
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    //跟NIO有关的服务启动初始化方法的实现
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) {
+                        ch.pipeline().addLast(new StringDecoder());
+                        ch.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
+                            //重写读动作
+                            @Override
+                            protected void channelRead0(ChannelHandlerContext ctx, String msg) {
+                                System.out.println(msg);
+                            }
+                        });
+                    }
+                });
+        bind(serverBootstrap, 8000);
+    }
+
+    private static void bind(final ServerBootstrap serverBootstrap, final int port){
+
+        //监听某个端口，返回值是一个ChannelFuture，是一个异步方法
+        serverBootstrap
+                .bind(port)
+                // 用来返回端口是否绑定成功
+                .addListener(new GenericFutureListener<Future<? super Void>>() {
+                    public void operationComplete(Future<? super Void> future) throws Exception {
+                        if(future.isSuccess()){
+                            System.out.println(port + " : 端口绑定成功!");
+                        }else{
+                            //如果绑定失败，继续找下一个可以绑定的端口
+                            System.out.println(port + " : 端口绑定失败!");
+                            bind(serverBootstrap, port+1);
+                        }
+                    }
+                });
+    }
+}
